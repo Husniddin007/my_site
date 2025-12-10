ebb8939f4fc402dad0dba458fd863e35d7c1374e
Husniddin007 <goffarovxusniddin@gmail.com>
2025-12-10 10:11:15 +0500

    taggit by TaggitableManager

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from taggit.managers import TaggableManager
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
@@ -34,6 +35,7 @@ class Post(models.Model):
     )
     objects = models.Manager()
     published = PublishedManger()
+    tags = TaggableManager()
 
     class Meta:
         ordering = ['-publish']
@@ -46,7 +48,7 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return reverse(
-            'blog:post_detali',
+            'blog:post_detail',
             args=[
                 self.publish.year,
                 self.publish.month,
@@ -54,3 +56,24 @@ class Post(models.Model):
                 self.slug
             ]
         )
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    name = models.CharField(max_length=250)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+    def __str__(self):
+        return f'Commnet by {self.name} on {self.post}'
+    
